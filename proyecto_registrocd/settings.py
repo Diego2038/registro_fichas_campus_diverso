d1304c9efffa890b590b077268fd3574a022f517
Juan Diego Gil T <juan.gil.trujillo@correounivalle.edu.co>
2023-06-01 15:26:31 -0500

    add environtment variables in project's database settings

--- proyecto_registrocd/settings.py
+++ proyecto_registrocd/settings.py
@@ -47,6 +47,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_registro',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -85,8 +87,12 @@ WSGI_APPLICATION = 'proyecto_registrocd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': variable_name_database,
+        'USER': variable_user,
+        'PASSWORD': variable_password,
+        'HOST': variable_host,
+        'PORT': variable_port,
     }
 }
 
