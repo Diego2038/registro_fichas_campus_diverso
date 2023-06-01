8e03e9063e4f3c61c2f3775e93a6f52cd6b54fb2
Juan Diego Gil T <juan.gil.trujillo@correounivalle.edu.co>
2023-06-01 15:34:16 -0500

    add person

--- app_registro/models.py
+++ app_registro/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Persona(models.Model):
+    id_persona = models.AutoField(primary_key=True)
+    incluir_correo_en_respuesta = models.BooleanField(default=False)
+    nombre_identitario = models.CharField(max_length=150)
+    nombre_y_apellido = models.CharField(max_length=150)
+    tipo_documento = models.CharField(max_length=50)
+    numero_documento = models.CharField(max_length=30)
+    fecha_nacimiento = models.DateField()
+    estrato_socioeconomico = models.IntegerField()
+    municipio_nacimiento = models.CharField(max_length=50)
+    departamento_nacimiento = models.CharField(max_length=50)
+    pais_nacimiento = models.CharField(max_length=50)
+    municipio_residencia = models.CharField(max_length=100)
+    zona_residencial = models.CharField(max_length=100)
+    direccion_residencia = models.CharField(max_length=200)
+    barrio_residencia = models.CharField(max_length=150)
+    comuna_barrio = models.CharField(max_length=20)
+    telefono = models.CharField(max_length=20)
+    estado_civil = models.CharField(max_length=30)
+    identidad_etnico_racial = models.CharField(max_length=70)
