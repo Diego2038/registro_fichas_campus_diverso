4cc25a1a6f9f07879ff6ebcffade8551c49ac183
Juan Diego G <juan.gil.trujillo@correounivalle.edu.co>
2023-06-03 17:09:45 -0500

    fix: delete, put y get a un item en particular, ahora está permitido realizar dichas peticiones con el número de documento de la persona

--- app_diversidad_sexual/views.py
+++ app_diversidad_sexual/views.py
@@ -5,12 +5,40 @@ from rest_framework import generics
 from .models import DiversidadSexual
 from .serializers import DiversidadSexualSerializer
 
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework import status
+from app_registro.models import Persona 
+
 # Persona
 class DiversidadSexualListCreateView(generics.ListCreateAPIView):
     queryset = DiversidadSexual.objects.all()
     serializer_class = DiversidadSexualSerializer
 
+
 class DiversidadSexualRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = DiversidadSexual.objects.all()
     serializer_class = DiversidadSexualSerializer
     lookup_field = 'id_persona'
+    
+    def get_object(self):
+        id_persona = self.kwargs['id_persona']
+        persona = get_object_or_404(Persona, numero_documento=id_persona)
+        diversidad_sexual = get_object_or_404(DiversidadSexual, id_persona=persona)
+        return diversidad_sexual
+    
+    def put(self, request, *args, **kwargs):
+        id_persona = self.kwargs['id_persona']
+        persona = get_object_or_404(Persona, numero_documento=id_persona)
+        diversidad_sexual = get_object_or_404(DiversidadSexual, id_persona=persona)
+        serializer = self.get_serializer(diversidad_sexual, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response(serializer.data)
+    
+    def delete(self, request, *args, **kwargs):
+        id_persona = self.kwargs['id_persona']
+        persona = get_object_or_404(Persona, numero_documento=id_persona)
+        diversidad_sexual = get_object_or_404(DiversidadSexual, id_persona=persona)
+        self.perform_destroy(diversidad_sexual)
+        return Response(status.HTTP_204_NO_CONTENT)
--- app_registro/urls.py
+++ app_registro/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import PersonaListCreateView, PersonaRetrieveUpdateDestroyView, PertenenciaGrupoPoblacionalListCreateView, PertenenciaGrupoPoblacionalRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path('personas/', PersonaListCreateView.as_view(), name='persona-list-create'),
-    path('personas/<str:numero_documento>/', PersonaRetrieveUpdateDestroyView.as_view(), name='persona-retrieve-update-destroy'),
+    path('', PersonaListCreateView.as_view(), name='persona-list-create'),
+    path('<str:numero_documento>/', PersonaRetrieveUpdateDestroyView.as_view(), name='persona-retrieve-update-destroy'),
     path('pertenencia_grupo_poblacional/', PertenenciaGrupoPoblacionalListCreateView.as_view(), name='pertenencia-grupo-poblacional-list-create'),
     path('pertenencia_grupo_poblacional/<str:pk>/', PertenenciaGrupoPoblacionalRetrieveUpdateDestroyView.as_view(), name='pertenencia-grupo-poblacional-retrieve-update-destroy'),
 ]
